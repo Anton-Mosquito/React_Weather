219e3e96687aafec4fce954adce21e069c9d8536
Anton-Mosquito <69711709+Anton-Mosquito@users.noreply.github.com>
2022-04-13 09:31:15 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
 } from "react-router-dom";
 import { FourHundredFour } from "./components/404";
@@ -10,12 +9,8 @@ import { Main } from "./components/main";
 export const App: React.FC = () => {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
+          <Route exact path="/" component={Main} />
           <Route path="*" component={FourHundredFour} />
-        </Switch>
     </Router>
   );
 }
