20c1e6c2d0d46e0b08608bb8d88eec34407bbba1
Anton Komarnytskyi <anton0886@outlook.com>
2026-01-19 14:15:20 +0200

    chore: remove deprecated .eslintignore; use ignores in eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+/* ESLint flat config for ESLint v9+ */
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'build/**', 'dist/**', 'public/**', '.cache/**', '.git/**'],
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+      prettier: require('eslint-plugin-prettier')
+    },
+    settings: {
+      react: { version: 'detect' }
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+    }
+  }
+];
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -4,9 +4,9 @@ import ChangeContext from '../models/context.model';
 export const Context = createContext<ChangeContext>({
   dataCity: undefined,
   dataPosition: undefined,
-  updateData: (value) => console.warn('default'),
+  updateData: (_value) => console.warn('default'),
   loadingCards: false,
-  updateWeatherCards: (value) => console.warn('default'),
+  updateWeatherCards: (_value) => console.warn('default'),
   loadingMain: false,
   typeRequset: '',
   trueInfo: true,
